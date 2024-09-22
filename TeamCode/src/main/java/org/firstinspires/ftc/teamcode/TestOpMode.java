93ef405a4afafffeea40b1417191d99405372ff7
RH809 <nayr.oaish@gmail.com>
2024-09-22 11:14:02 -0700

    Changed gradle files for command based
    Edited build.common.gradle and TeamCode's build.gradle so that we can use command based programming.
    Also added PurePursuitSample.java from the github examples for testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.arcrobotics.ftclib.command.SubsystemBase;
 
 
 @TeleOp(name="Basic: Linear OpMode", group="Linear OpMode")
